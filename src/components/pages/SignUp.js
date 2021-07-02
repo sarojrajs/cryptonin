12a26b09f7b246d2d78ab3fa665220e6fc4f43f0
siddharth rout <srout5096@gmail.com>
2021-07-02 22:24:59 +0530

    Till nav and sign in change

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,53 +1,59 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import './SignUp.css'
 import '../../App.css';
 import { auth } from '../../firebase';
 
 export default function SignUp() {
-  const [email,setEmail]=useState('')
-  const [password,setPassword]=useState('')
-  const history=useHistory()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const history = useHistory()
 
-  const resetValues=()=>{
+  const resetValues = () => {
     setEmail('')
     setPassword('')
   }
-  
-  const loginHandler=()=>{
-    console.log(email,password)
-    auth.signInWithEmailAndPassword(email,password).then((response)=>{
+
+  const loginHandler = () => {
+    console.log(email, password)
+    auth.signInWithEmailAndPassword(email, password).then((response) => {
       console.log(response)
-      
-    }).catch(err=>{
+
+    }).catch(err => {
       alert(err.message)
       console.log(err)
     })
     resetValues()
   }
 
-  auth.onAuthStateChanged((user)=>{
-    if(user){
+  auth.onAuthStateChanged((user) => {
+    if (user) {
       history.push('/coin-page')
     }
   })
 
   return (
-    <div>
-      <div>
-        <p>Email</p>
-        <input type='email' placeholder='Email' value={email} onChange={(e)=>{
-          setEmail(e.target.value)
-        }}/>
+    <div >
+      <div className='Login'>
+        <div className="sign-in">
+          <h1>Login</h1>
+          <p>Email</p>
+          <input type='email' placeholder='Email' value={email} onChange={(e) => {
+            setEmail(e.target.value)
+          }} />
+
+          <p>Password</p>
+          <input type='password' placeholder='Password'
+            value={password} onChange={(e) => {
+              setPassword(e.target.value)
+            }}
+          />
+          <div>
+            <button onClick={loginHandler}>Sign In</button>
+          </div>
+        </div>
       </div>
-      <div>
-        <p>Password</p>
-        <input type='password' placeholder='Password' 
-        value={password} onChange={(e)=>{
-          setPassword(e.target.value)
-        }}
-        />
-      </div>
-      <button onClick={loginHandler}>Sign In</button>
     </div>
+
   )
 }
